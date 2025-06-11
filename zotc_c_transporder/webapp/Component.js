61a4828ae50f3469cefc744200b9e49f066728ea
andrezzapp <andrezzapaivaperes@hotmail.com>
2025-06-11 15:42:50 -0300

    primeiro commit

---
+++ zotc_c_transporder/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("br.numen.zotcctransporder.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
